2203b299fbde9553ae11c4baa44b87dc97e2f4c5
Abdur Rakib Rony <65207907+ronyfr3@users.noreply.github.com>
2020-09-14 06:44:26 +0600

    Add files via upload

---
+++ src/Sidebar.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import * as FaIcons from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { SidebarData } from './SidebarData';
+import './SideBar.css'
+import { IconContext } from 'react-icons';
+
+function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <div className='navbar'>
+          <Link to='#' className='menu-bars'>
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>
+        </div>
+
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+            <ul className='nav-menu-items' onClick={showSidebar}>
+             <li className='navbar-toggle'>
+                <Link to='#' className='menu-bars'>
+                  <span className='_text'>Hello, Abdur</span>
+                  <span className='_icon'>X</span>
+                  <hr className='line'></hr>
+                </Link>
+             </li>
+
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                    <span className='arrow_sign'>{item.arrow_sign}</span>
+                  </Link>
+                </li>
+              );
+            })}
+
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>
+  );
+}
+
+export default Navbar;
